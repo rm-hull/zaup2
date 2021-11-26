ac60a75e1dba3f7001bb707baed5014f33936252
Richard Hull <rm_hull@yahoo.co.uk>
2021-11-26 17:52:53 +0000

    Calculate time left on initial render rather than assuming always 30 seconds

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -10,16 +10,17 @@ type CountdownTimerProps = {
 const getColor = (timeLeft: number) => {
   if (timeLeft > 10 || timeLeft === 0) return "green.300";
   else if (timeLeft > 5) return "orange.300";
-  else return "red.300";
+  else return "red.400";
+};
+
+const calcTimeLeft = (duration: number) => {
+  const seconds = Math.floor(Date.now() / 1000) % 60;
+  return duration - 1 - (seconds % duration);
 };
 
 export default function CountdownTimer({ duration }: CountdownTimerProps): JSX.Element {
-  const [timeLeft, setTimeLeft] = useState<number>(duration);
-  useHarmonicIntervalFn(() => {
-    const seconds = Math.floor(Date.now() / 1000) % 60;
-    const timeLeft = duration - 1 - (seconds % duration);
-    setTimeLeft(timeLeft);
-  }, 1000);
+  const [timeLeft, setTimeLeft] = useState<number>(calcTimeLeft(duration));
+  useHarmonicIntervalFn(() => setTimeLeft(calcTimeLeft(duration)), 1000);
 
   return (
     <CircularProgress value={(timeLeft * 100) / duration} color={getColor(timeLeft)} size="70px" thickness="12px">
