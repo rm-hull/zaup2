156ab6f07dc9b768c61f562faaecad834883842d
Richard Hull <rm_hull@yahoo.co.uk>
2021-11-26 17:06:11 +0000

    Small fix

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar({ children }: { children: ReactNode }): JSX.Elem
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
-      <SidebarContent onClose={() => onClose} display={{ base: "none", md: "block" }} />
+      <SidebarContent onClose={onClose} display={{ base: "none", md: "block" }} />
       <Drawer
         // eslint-disable-next-line jsx-a11y/no-autofocus
         autoFocus={false}
