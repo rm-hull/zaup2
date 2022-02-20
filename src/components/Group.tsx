8572319920fe682e0751a6b55d83679497aa0fec
Richard Hull <rm_hull@yahoo.co.uk>
2022-02-20 14:11:58 +0000

    Force refresh if overdue

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -15,9 +15,11 @@ export default function Group({ filter = () => true }: GroupProps): JSX.Element 
   const { data } = useOtpParameters();
   const [refresh, setRefresh] = useState<number | undefined>(undefined);
   useHarmonicIntervalFn(() => {
-    const seconds = Math.floor(Date.now() / 1000) % 60;
+    const now = Date.now();
+    const seconds = Math.floor(now / 1000) % 60;
     const timeLeft = 29 - (seconds % 30);
-    setRefresh(timeLeft === 0 ? Date.now() : undefined);
+    const overdue = now - (refresh ?? 0) > 40000;
+    setRefresh(timeLeft === 0 || overdue ? now : undefined);
   }, 1000);
 
   const filtered = useMemo(() => sort(data)?.filter(filter), [data, filter]);
