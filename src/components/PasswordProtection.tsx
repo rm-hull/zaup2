2149f137c732dbad334482a939d54508bc90555c
Richard Hull <rm_hull@yahoo.co.uk>
2023-11-04 23:18:23 +0000

    No need for open/close disclosure on password protection

--- src/components/PasswordProtection.tsx
+++ src/components/PasswordProtection.tsx
@@ -1,6 +1,4 @@
-import { useDisclosure } from "@chakra-ui/react";
 import useGeneralSettings from "../hooks/useGeneralSettings";
-import { useEffect } from "react";
 import { PasswordModal } from "./PasswordModal";
 import usePassword from "../hooks/usePassword";
 
@@ -10,20 +8,16 @@ export default function PasswordProtection({
   children,
 }: React.PropsWithChildren<PasswordProtectionProps>): JSX.Element {
   const [settings, updateSettings] = useGeneralSettings();
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const [password, setPassword] = usePassword();
-
-  useEffect(onOpen, [onOpen]);
 
   const handleSubmit = (enteredPassword: string) => {
     updateSettings({ ...settings, encrypted: true });
     setPassword(enteredPassword);
-    onClose();
   };
 
   if (password !== undefined) {
     return <>{children}</>;
   }
 
-  return <PasswordModal isOpen={isOpen} onSubmit={handleSubmit} confirm={!settings?.encrypted} />;
+  return <PasswordModal isOpen onSubmit={handleSubmit} confirm={!settings?.encrypted} />;
 }
