43a254e2acb6df6c6dfa1aa0fd6de92a337b2ea6
Richard Hull <rm_hull@yahoo.co.uk>
2023-11-05 00:11:38 +0000

    Password input with show/hide feature

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,18 @@
+import { InputGroup, Input, InputRightElement, Button, InputProps } from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function PasswordInput(inputProps: InputProps): JSX.Element {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow((prev) => !prev);
+
+  return (
+    <InputGroup size="md">
+      <Input {...inputProps} pr="4.5rem" type={show ? "text" : "password"} />
+      <InputRightElement width="4.5rem">
+        <Button h="1.75rem" size="sm" onClick={handleClick}>
+          {show ? "Hide" : "Show"}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+}
