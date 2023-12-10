d1d72923e842a1af8fc86a5489acbb212daaa806
Richard Hull <rm_hull@yahoo.co.uk>
2023-12-10 19:17:29 +0000

    Update dependencies

--- src/api/ipify.ts
+++ src/api/ipify.ts
@@ -1,10 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
 interface IpifyResponse {
-  ip: string
-};
+  ip: string;
+}
 
 export async function ipAddress(): Promise<string> {
-  const resp = await axios.get<IpifyResponse>("https://api.ipify.org?format=json")
-  return resp.data.ip
+  const resp = await axios.get<IpifyResponse>("https://api.ipify.org?format=json");
+  return resp.data.ip;
 }
