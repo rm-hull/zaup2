aac4522188430b41c53dd8e8bb441a30838ead7e
Richard Hull <rm_hull@yahoo.co.uk>
2025-05-15 22:43:37 +0100

    Update dependencies

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -3,9 +3,8 @@ import { Metric } from "web-vitals";
 export const reportWebVitals = (onPerfEntry?: (metric: Metric) => void): void => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals")
-      .then(({ onCLS, onFID, onINP, onFCP, onLCP, onTTFB }) => {
+      .then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
         onCLS(onPerfEntry);
-        onFID(onPerfEntry);
         onINP(onPerfEntry);
         onFCP(onPerfEntry);
         onLCP(onPerfEntry);
