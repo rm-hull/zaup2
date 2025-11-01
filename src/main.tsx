d87fe0c14a4f774d44fa28d9dd72ccec37d3a491
Richard Hull <rm_hull@yahoo.co.uk>
2025-11-01 15:23:36 +0000

    Refactor imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import ReactGA from "react-ga4";
