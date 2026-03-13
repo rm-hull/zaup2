438eefdad85ed0f29f172bcd16a808326f3e84d1
Richard Hull <rm_hull@yahoo.co.uk>
2026-03-13 12:52:46 +0000

    refactor: migrate to native Vite tsconfig support
    - Updated `@vitejs/plugin-react` to `v6.0.1`.
    - Removed `vite-tsconfig-paths` dependency.
    - Enabled native `tsconfigPaths` resolution via Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
 import { execSync } from "child_process";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
@@ -10,10 +9,11 @@ export default defineConfig(() => {
   process.env.VITE_GIT_COMMIT_HASH = execSync("git describe --always --dirty").toString().trimEnd();
 
   return {
-    plugins: [react({babel: {plugins: ["babel-plugin-react-compiler"]}}), tsconfigPaths()],
+    plugins: [react({ babel: { plugins: ["babel-plugin-react-compiler"] } })],
     base: "/zaup2",
     build: {
-      sourcemap: true
-    }
+      sourcemap: true,
+    },
+    resolve: { tsconfigPaths: true },
   };
 });
